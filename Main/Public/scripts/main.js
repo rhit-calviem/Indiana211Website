4cde1245d30c55777daa8b9f52d8e5aa84e5ab83
calviem <calviem@rose-hulman.edu>
2023-10-09 08:12:00 -0400

    added header+footer with method to get date

--- Main/Public/scripts/main.js
+++ Main/Public/scripts/main.js
@@ -14,3 +14,17 @@
          caseManagerDropdown.style.display = "none";
      }
  });
+
+ function updateCurrentDate() {
+    const currentDate = new Date();
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1;
+    const year = currentDate.getFullYear();
+
+    const formattedDate = `${month}/${day}/${year}`;
+
+    document.getElementById("print-date").textContent = formattedDate;
+}
+
+updateCurrentDate();
+setInterval(updateCurrentDate, 86400000);
