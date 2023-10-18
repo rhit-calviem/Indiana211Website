3b5b16fa2a45e31cffd5bd9b0b0179d0e45217ac
calviem <calviem@rose-hulman.edu>
2023-10-18 19:57:07 -0400

    nested checkboxes

--- Main/Public/scripts/main.js
+++ Main/Public/scripts/main.js
@@ -1,20 +1,22 @@
- // JavaScript code for handling the nested dropdown
- var menu = document.getElementById("menu");
- var caseManagerDropdown = document.getElementById("caseManagersDropdown");
- var caseManagerOptions = document.getElementById("caseManagerOptions");
+//  // JavaScript code for handling the nested dropdown
+//  var menu = document.getElementById("menu");
+//  var caseManagerDropdown = document.getElementById("caseManagersDropdown");
+//  var caseManagerOptions = document.getElementById("caseManagerOptions");
 
- // Hide the nested dropdown initially
- caseManagerDropdown.style.display = "none";
+//  // Hide the nested dropdown initially
+//  caseManagerDropdown.style.display = "none";
 
- // Show the nested dropdown when the "Case Managers" option is selected
- menu.addEventListener("change", function() {
-     if (menu.value === "case_managers") {
-         caseManagerDropdown.style.display = "block";
-     } else {
-         caseManagerDropdown.style.display = "none";
-     }
- });
+//  // Show the nested dropdown when the "Case Managers" option is selected
+//  menu.addEventListener("change", function() {
+//      if (menu.value === "case_managers") {
+//          caseManagerDropdown.style.display = "block";
+//      } else {
+//          caseManagerDropdown.style.display = "none";
+//      }
+//  });
 
+
+ // Get updated time for header/ footer
  function updateCurrentDate() {
     const currentDate = new Date();
     const day = currentDate.getDate();
@@ -25,7 +27,13 @@
 
     document.getElementById("print-date").textContent = formattedDate;
 }
-
 updateCurrentDate();
 setInterval(updateCurrentDate, 86400000);
+
+
+// Nested checkbox
+document.getElementById("basic-needs").addEventListener("change", function () {
+    var nestedCheckboxes = document.querySelector(".nested-checkboxes");
+    nestedCheckboxes.style.display = this.checked ? "block" : "none";
+});
 
