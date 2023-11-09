6ecfe9b63f66bb52bf362854122eb9d3a082cf01
Blaise Swartwood <swartwba@rose-hulman.edu>
2023-11-09 13:21:28 -0500

    Finished project

--- Main/server.js
+++ Main/server.js
@@ -5,6 +5,13 @@ app.use(express.static('Public'));
 
 const dataFilePath = './data/realResources.json';
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins (not recommended for production)
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/data', async (req, res) => {
   try {
     const data = await fs.readFile(dataFilePath, 'utf-8');
@@ -21,9 +28,3 @@ app.listen(port, () => {
 });
 
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*'); // Allow all origins (not recommended for production)
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
